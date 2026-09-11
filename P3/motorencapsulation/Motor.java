b61ad79955ccffa05e299c67d16ac338b2c04892
yunus <yunusfikr07@gmail.com>
2026-09-11 09:25:41 +0700

    P3_Percobaan2

--- P3/motorencapsulation/Motor.java
+++ P3/motorencapsulation/Motor.java
@@ -1,8 +1,32 @@
 package P3.motorencapsulation;
 
 public class Motor {
-    public  int kecepatan = 0;
-    public boolean kontakOn = false;
+    private  int kecepatan = 0;
+    private  boolean kontakOn = false;
+
+    public void nyalakanMesin(){
+        kontakOn = true;
+    }
+    public void matikanMesin(){
+        kontakOn = false;
+        kecepatan = 0;
+    }
+    public void tambahKecepatan(){
+        if (kontakOn == true) {
+            kecepatan += 5;
+        }
+        else{
+            System.out.println("Kecepatan tidak bisa bertambah karena Mesin Off! \n");
+        }
+    }
+    public void kurangiKecepatan(){
+        if (kontakOn == true) {
+            kecepatan -= 5;
+        }
+        else{
+            System.out.println("Kecepatan tidak bisa berkurang karena Mesin Off! \n");
+        }
+    }
 
     public  void printStatus(){
         if (kontakOn == true){
--- P3/motorencapsulation/MotorDemo.java
+++ P3/motorencapsulation/MotorDemo.java
@@ -4,7 +4,21 @@ public class MotorDemo {
     public static void main(String[] args) {
         Motor motor = new Motor();
         motor.printStatus();
-        motor.kecepatan = 50;
+        motor.tambahKecepatan();
+
+        motor.nyalakanMesin();
+        motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        motor.matikanMesin();
         motor.printStatus();
     }
 }
