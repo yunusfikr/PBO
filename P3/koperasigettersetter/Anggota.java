7756bf302c4dc2c6526b0c270797b072a21fb95b
yunus <yunusfikr07@gmail.com>
2026-09-11 09:35:48 +0700

    P3_Percobaan3

---
+++ P3/koperasigettersetter/Anggota.java
@@ -0,0 +1,29 @@
+package P3.koperasigettersetter;
+
+public class Anggota {
+    private String nama;
+    private String alamat;
+    private float simpanan;
+
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+    public void setAlamat(String alamat){
+        this.alamat = alamat;
+    }
+    public String getNama(){
+        return nama;
+    }
+    public String getAlamat(){
+        return alamat;
+    }
+    public float getSimpanan(){
+        return simpanan;
+    }
+    public void setor(float uang){
+        simpanan +=uang;
+    }
+    public void pinjam(float uang){
+        simpanan -=uang;
+    }
+}
