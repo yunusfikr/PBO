f7ff33d96c853bec8d3d14e12c9b2912eb1d9714
yunus <yunusfikr07@gmail.com>
2026-09-11 09:40:42 +0700

    P3_Percobaan4

--- P3/koperasigettersetter/Anggota.java
+++ P3/koperasigettersetter/Anggota.java
@@ -5,6 +5,12 @@ public class Anggota {
     private String alamat;
     private float simpanan;
 
+    Anggota(String nama, String alamat){
+        this.nama = nama;
+        this.alamat = alamat;
+        this.simpanan = 0;
+    }
+
     public void setNama(String nama){
         this.nama = nama;
     }
--- P3/koperasigettersetter/KoperasiDemo.java
+++ P3/koperasigettersetter/KoperasiDemo.java
@@ -2,7 +2,9 @@ package P3.koperasigettersetter;
 
 public class KoperasiDemo {
     public static void main(String[] args) {
-        Anggota anggota1 = new Anggota();
+        Anggota anggota1 = new Anggota("Iwan", "Jalan Mawar");
+        System.out.println("Simpanan " +anggota1.getNama()+ " : Rp"+ anggota1.getSimpanan());
+
         anggota1.setNama("Iwan Setiawan");
         anggota1.setAlamat("Jalan Sukarno Hatta no 10");
         anggota1.setor(100000);
