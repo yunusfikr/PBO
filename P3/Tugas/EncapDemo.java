6ca26ee8af242837f24863d70055ee098503220c
yunus <yunusfikr07@gmail.com>
2026-09-16 16:46:45 +0700

    P3_TugasNo3

--- P3/Tugas/EncapDemo.java
+++ P3/Tugas/EncapDemo.java
@@ -19,6 +19,8 @@ public class EncapDemo {
     public void setAge(int newAge){
         if (newAge > 30) {
             age = 30;
+        }else if (newAge < 18) {
+            age = 18;
         }
         else{
             age = newAge;
